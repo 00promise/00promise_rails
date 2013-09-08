0d79468306e9931793cc466dc64521d5973ce1d3
강준수 <joonsoo.kang@gmail.com>
2013-09-08 15:54:14 +0900

    - devise.rb에 config.secret_key 추가 - schema.rb & seeds.rb 업데이트

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130908053117) do
+ActiveRecord::Schema.define(:version => 20130908061033) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(:version => 20130908053117) do
 
   create_table "positions", :force => true do |t|
     t.string   "name"
-    t.string   "type"
+    t.string   "type_name"
     t.integer  "type_code"
     t.string   "district"
     t.datetime "created_at", :null => false
