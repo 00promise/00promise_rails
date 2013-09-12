320a33703be4696faf3c5f355759da246c664147
강준수 <joonsoo.kang@gmail.com>
2013-09-12 22:21:58 +0900

    timezone을 Seoul로 변경

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Promise
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = "Seoul"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -47,6 +47,8 @@ module Promise
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
+
+    config.active_record.default_timezone = :local
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
