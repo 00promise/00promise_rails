f68be43e00beccaa2e8a6e61eee4809444ff95d5
강준수 <joonsoo.kang@gmail.com>
2013-09-16 20:17:57 +0900

    ratings 변경에 따라 manifestos 테이블의 rating_cnt가 변경되도록 수정

--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -6,14 +6,13 @@ class Api::RatingsController < ApplicationController
     hash[:user_id] = current_user.id
     hash[:manifesto_id] = params[:manifesto_id]
 
-    @rating = Rating.find_or_create_by_user_id_and_manifesto_id(hash)
+    @rating = Rating.where(hash).first_or_initialize
     @rating.grade = params[:grade]
     @rating.save
   end
 
   def destroy
     @rating = Rating.find_by_user_id_and_manifesto_id(current_user.id, params[:manifesto_id])
-    logger.info { @rating }
-    @rating && @rating.delete
+    @rating && @rating.destroy
   end
 end
--- app/models/manifesto.rb
+++ app/models/manifesto.rb
@@ -4,4 +4,18 @@ class Manifesto < ActiveRecord::Base
   has_many :ratings
   has_many :replies
   has_one :latest_reply, class_name: "Reply", order: "id DESC"
+
+  def increment_with_sql!(attribute, by = 1)
+    raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
+    original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
+    self.class.update_all("#{attribute} = #{original_value_sql} + #{by.to_i}", "id = #{id}")
+    reload
+  end
+
+  def decrement_with_sql!(attribute, by = 1)
+    raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
+    original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
+    self.class.update_all("#{attribute} = #{original_value_sql} - #{by.to_i}", "id = #{id}")
+    reload
+  end
 end
