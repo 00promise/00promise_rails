d05d39860b88c66b6151e4451db6b85c34c9f8fc
강준수 <joonsoo.kang@gmail.com>
2013-09-28 03:02:33 +0900

    seeds 데이터가 자꾸 변경되는 이슈 대응

--- app/models/manifesto.rb
+++ app/models/manifesto.rb
@@ -1,21 +1,9 @@
 class Manifesto < ActiveRecord::Base
+  include Variation
+
   attr_accessible :description, :fair_cnt, :good_cnt, :politician_id, :poor_cnt, :reply_cnt, :title
   belongs_to :politician
   has_many :ratings
   has_many :replies
   has_one :latest_reply, class_name: "Reply", order: "id DESC"
-
-  def increment_with_sql!(attribute, by = 1)
-    raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
-    original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
-    self.class.update_all("#{attribute} = #{original_value_sql} + #{by.to_i}", "id = #{id}")
-    reload
-  end
-
-  def decrement_with_sql!(attribute, by = 1)
-    raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
-    original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
-    self.class.update_all("#{attribute} = #{original_value_sql} - #{by.to_i}", "id = #{id}")
-    reload
-  end
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,7 +2,7 @@ class Reply < ActiveRecord::Base
   include Variation
 
   attr_accessible :agree_cnt, :content, :disagree_cnt, :manifesto_id, :status, :user_id
-  belongs_to :manifesto, counter_cache: :reply_cnt
+  belongs_to :manifesto#TODO 주석제거, counter_cache: :reply_cnt
   belongs_to :user
   has_many :reply_evaluations
 end
