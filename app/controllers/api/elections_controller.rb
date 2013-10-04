9a9be0a570a66181d6583051271bbd7c862712bf
강준수 <joonsoo.kang@gmail.com>
2013-10-04 21:51:45 +0900

    API에 페이징 처리 추가 - GET elections/:id/politicians.json - GET parties/:id/politicians.json

--- app/controllers/api/elections_controller.rb
+++ app/controllers/api/elections_controller.rb
@@ -1,10 +1,15 @@
 class Api::ElectionsController < Api::BaseController
+  # GET elections.json
   def index
     @elections = Election.where("enabled = ?", true).order("ord")
   end
 
+  # GET elections/:id/politicians.json
   def politicians
+    count = params[:count] || 20
+    page = params[:page] || 1
+
     election = Election.find(params[:id])
-    @politicians = election.politicians.order("name").limit(30)
+    @politicians = election.politicians.order("name").page(page).per(count)
   end
 end
