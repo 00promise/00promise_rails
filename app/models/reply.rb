884dcdda07ff3bb0e39d483284a9e3c28feda7cf
강준수 <joonsoo.kang@gmail.com>
2013-10-12 14:08:18 +0900

    댓글 node에 user_eval_type 속성 추가

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -3,7 +3,7 @@ class Reply < ActiveRecord::Base
   after_initialize :set_virtual_attributes
 
   attr_accessible :agree_cnt, :content, :disagree_cnt, :manifesto_id, :status, :user_id
-  attr_accessor :is_best, :is_owner
+  attr_accessor :is_best, :is_owner, :user_eval_type
   belongs_to :manifesto, counter_cache: :reply_cnt
   belongs_to :user
   has_many :reply_evaluations
@@ -11,9 +11,17 @@ class Reply < ActiveRecord::Base
   def set_virtual_attributes
     @is_best = false
     @is_owner = false
+    @user_eval_type = nil
 
-    if User.current_user and User.current_user.id == self.user_id
-      @is_owner = true
+    if User.current_user
+      if User.current_user.id == self.user_id
+        @is_owner = true
+      end
+
+      user_evaluation = self.reply_evaluations.where("user_id = ?", User.current_user.id).first
+      if user_evaluation
+        @user_eval_type = user_evaluation.eval_type
+      end
     end
   end
 end
