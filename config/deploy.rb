c3a9a901f24f1c87a51bae8321e6b11902bf9fbf
Rangken <rangken@gmail.com>
2014-09-16 12:09:20 +0900

    remove csv

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :scm, :git
 set :branch, "develop" #cap -S branch=branchname deploy
 set :keep_releases, 5
 set :user, "root"
-set :rails_env, "development"
+set :rails_env, "production"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
